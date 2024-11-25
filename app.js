9403dda537a0e1e87a97ac8091451539f7b6c6bd
Iván Blanco <ivanblanco4m@gmail.com>
2024-11-25 15:33:46 +0100

    routes tested

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-// IMPORT PACKAGES
-// Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require("express")
+const logger = require("morgan")
 
+const app = express()
 
+app.use(express.json())
+app.use(logger("dev"))
 
-// CREATE EXPRESS APP
-// Here you should create your Express app:
+app.get("/api/projects", (req, res) => {
+    const projectsData = require("./data/projects.json")
+    res.json(projectsData)
+})
 
+app.get("/api/articles", (req, res) => {
+    const articlesData = require("./data/articles.json")
+    res.json(articlesData)
+})
 
+app.get("*", (req, res) => {
+    res.sendStatus(404)
+})
 
-// MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
-
-
-
-// ROUTES
-// Start defining your routes here:
-
-
-
-// START THE SERVER
-// Make your Express server listen on port 5005:
+app.listen(5005, () => console.log("levantese"))
